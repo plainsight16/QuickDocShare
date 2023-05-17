3415d6eab0bf163ad4f4c63fbc11c41ac3a7cc2c
Timilehin Bakare <timmybaks2002@gmail.com>
2023-05-17 22:14:26 +0100

    Completed DocHandler

--- DocRepresentation/Program.cs
+++ DocRepresentation/Program.cs
@@ -7,26 +7,7 @@ namespace DocRepresentation
     {
         static void Main(string[] args)
         {
-            // I'm just testing the normalizer, both should normalize to the same word - send
-            //Tokenize.Normalize("Sending");
-            //Tokenize.Normalize("SENDS");
-
-            DocumentHandler doc_handler = new SlideShowHandler(null);
-            string doc_1_texts = doc_handler.parseDocument("pptx", @"C:\Users\Julius Alibrown\Desktop\class\Project\search-engine\DocHandler\powerpoint_file.pptx");
-
-            DocumentHandler doc_handler2 = new SlideShowHandler(null);
-            string doc_2_texts = doc_handler2.parseDocument("pptx", @"C:\Users\Julius Alibrown\Desktop\class\Project\search-engine\DocHandler\powerpoint_file_2.pptx");
-
-            List<string> doc_texts = new List<string>
-            {
-                // DOC 1
-                doc_1_texts,
-
-                // DOC 2
-                doc_2_texts
-            };
-
-
+            List<string> doc_texts = new DocumentHandler(@"..\.\Files").GetDocTexts();
             new DocRepresentation(doc_texts);
         }
     }
