4c9e9182554f5cac1b2ca143764289c5d74ee4ec
Tobi Adeniji <adenijitobi94@gmail.com>
2023-05-22 12:56:44 +0100

    test for DocParser

--- DocHandler/Tests/DocHandlerTests.cs
+++ DocHandler/Tests/DocHandlerTests.cs
@@ -8,8 +8,54 @@ using System.Threading.Tasks;
 namespace DocHandler.Tests
 {
     [TestFixture]
-    internal class DocHandlerTests
+    public class DocParserTests
     {
+        private DocParser parser;
 
+        [SetUp]
+        public void Setup()
+        {
+            parser = new DocParser();
+        }
+
+        [Test]
+        public void CanParse_ValidDocFile_ReturnsTrue()
+        {
+            // Arrange
+            string filePath = "C:\\path\\to\\valid.doc";
+
+            // Act
+            bool result = parser.CanParse(filePath);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void CanParse_InvalidDocFile_ReturnsFalse()
+        {
+            // Arrange
+            string filePath = "C:\\path\\to\\invalid.txt";
+
+            // Act
+            bool result = parser.CanParse(filePath);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void ParseDocument_ValidDocFile_ReturnsDocumentContent()
+        {
+            // Arrange
+            string filePath = "C:\\path\\to\\document.doc";
+            string expectedContent = "This is the content of the document.";
+
+            // Act
+            string result = parser.parseDocument(filePath);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expected: expectedContent));
+        }
     }
 }
